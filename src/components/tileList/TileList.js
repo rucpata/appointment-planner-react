f6b10fbbb793be8bd67110cffb666ea68631d802
rucpata <rucinska.patrycja@gmail.com>
2021-06-15 13:13:46 +0200

    appointment page

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -5,9 +5,9 @@ import { Tile } from '../tile/Tile';
 export const TileList = ({tiles}) => {
   return (
     <div>
-      {tiles.map((tile, index) => {
+      {tiles.map((tile, index) => (
         <Tile key={index} tile={tile}/>
-      })}
+      ))}
     </div>
   );
 };
