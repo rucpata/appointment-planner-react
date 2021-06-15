27ba989ac9efdffc71a676a62c3e673a13aad9ec
rucpata <rucinska.patrycja@gmail.com>
2021-06-15 12:31:13 +0200

    Contact Form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,32 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        name="name" 
+        value={name}
+        onChange={(e)=>setName(e.target.value)}
+        required
+        placeholder='Nazwa Kontaktu'
+      />
+      <input 
+        type="tel"
+        name="phone"
+        value={phone}
+        onChange={(e)=>setPhone(e.target.value)}
+        required
+        placeholder='Numer telefonu' 
+      />
+      <input 
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}
+        required
+        placeholder='Adres email' 
+      />
+      <input type="submit" value="Dodaj kontakt" />
+    </form>
   );
 };
